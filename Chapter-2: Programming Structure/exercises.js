de99d2629ee97ff9c0c2e87b9d9d2a778100b400
Benjamin Huy Pham <benjaminhpham@gmail.com>
2022-12-28 13:34:31 -0700

    completed chessboard exe

--- Chapter-2: Programming Structure/exercises.js
+++ Chapter-2: Programming Structure/exercises.js
@@ -17,3 +17,20 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+
+//2.3 Chessboard
+const chessboard = (num) => {
+  for (let i = 0; i < num; i++) {
+    let initial = "";
+    for (let j = 0; j < num; j++) {
+      if ((i % 2 !== 0 && j % 2 !== 0) || (i % 2 === 0 && j % 2 === 0)) {
+        initial += " ";
+      } else if ((i % 2 === 0 && j % 2 !== 0) || (i % 2 !== 0 && j % 2 === 0)) {
+        initial += "#";
+      }
+    }
+    console.log(initial);
+  }
+};
+
+chessboard(8);
