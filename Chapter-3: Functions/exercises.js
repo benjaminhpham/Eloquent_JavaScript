d68e837f34ee250d93618f2f384d7d45b8a94e4b
Benjamin Huy Pham <benjaminhpham@gmail.com>
2022-12-29 00:21:50 -0700

    completed recursion exe

--- Chapter-3: Functions/exercises.js
+++ Chapter-3: Functions/exercises.js
@@ -7,3 +7,21 @@ const minNum = (num1, num2) => {
 };
 
 console.log(minNum(10, 30));
+
+// 3.2 Recursion
+const isEven = (num) => {
+  if (num < 0) {
+    return undefined;
+  }
+  if (num === 0) {
+    return true;
+  } else if (num === 1) {
+    return false;
+  } else {
+    return isEven(num - 2);
+  }
+};
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
