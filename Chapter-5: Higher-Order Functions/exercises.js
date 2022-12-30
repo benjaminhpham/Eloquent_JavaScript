bc5b3f0af2a9badb253c00eb5f5c2685bb9425db
Benjamin Huy Pham <benjaminhpham@gmail.com>
2022-12-30 16:58:12 -0700

    completed everything exe

--- Chapter-5: Higher-Order Functions/exercises.js
+++ Chapter-5: Higher-Order Functions/exercises.js
@@ -6,3 +6,21 @@ const arr = [
 ];
 
 console.log(arr.reduce((acc, curr) => acc.concat(curr)));
+
+// 5.3 Everything
+const everyWithLoop = (array, test) => {
+  for (let element of array) {
+    if (!test(element)) return false;
+  }
+  return true;
+};
+
+const everyWithoutLoop = (array, test) => {
+  return !array.some((element) => !test(element));
+};
+
+console.log(everyWithLoop([1, 2, 3, 4, 5], (e) => e % 2 === 0)); // false
+console.log(everyWithLoop([2, 4, 6, 8, 10], (e) => e % 2 === 0)); // true
+
+console.log(everyWithoutLoop([1, 2, 3, 4, 5], (e) => e % 2 === 0)); // false
+console.log(everyWithoutLoop([2, 4, 6, 8, 10], (e) => e % 2 === 0)); // true
